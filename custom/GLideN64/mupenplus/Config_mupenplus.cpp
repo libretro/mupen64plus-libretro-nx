420b78c68b6da6cb69c511816fe379f94bf70f94
Logan McNaughton <logan@bacoosta.com>
2016-06-20 19:43:20 -0600

    Fix desktop build

--- custom/GLideN64/mupenplus/Config_mupenplus.cpp
+++ custom/GLideN64/mupenplus/Config_mupenplus.cpp
@@ -11,7 +11,9 @@
 #include "../RSP.h"
 #include "../Log.h"
 extern "C" {
-#include "osal/files.h"
+#include <sys/stat.h>
+#include "osal_files.h"
+#include <dirent.h>
 }
 
 Config config;
@@ -19,4 +21,24 @@ Config config;
 void Config_LoadConfig()
 {
 	config.resetToDefaults();
+}
+
+EXPORT int CALL osal_path_existsW(const wchar_t *_path) {
+    char path[PATH_MAX];
+    wcstombs(path, _path, PATH_MAX);
+    struct stat fileinfo;
+    return stat(path, &fileinfo) == 0 ? 1 : 0;
+}
+
+EXPORT int CALL osal_is_directory(const wchar_t * _name) {
+    char name[PATH_MAX + 1];
+    wcstombs(name, _name, PATH_MAX);
+    DIR* dir;
+    dir = opendir(name);
+    if(dir != NULL)
+    {
+        closedir(dir);
+        return 1;
+    }
+    return 0;
 }
