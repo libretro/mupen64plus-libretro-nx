c4402a997f1e265b0fd9303f0466ded27354f35c
Logan McNaughton <logan@bacoosta.com>
2016-08-30 10:48:48 -0600

    I just can't get along with the RetroArch menu...

--- custom/GLideN64/CommonPluginAPI.cpp
+++ custom/GLideN64/CommonPluginAPI.cpp
@@ -62,7 +62,7 @@ EXPORT void CALL gln64ShowCFB (void)
 EXPORT void CALL gln64UpdateScreen (void)
 {
 	if (render == 1) {
-		if (!bound) {
+		if (!bound && EnableFBEmulation) {
 			glsm_ctl(GLSM_CTL_STATE_BIND, NULL);
 			bound = 1;
 		}
