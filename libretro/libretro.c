65cb737822e7ba023b963492e948462f50490682
Logan McNaughton <logan@bacoosta.com>
2016-10-11 14:14:54 -0600

    Add some higher resolutions

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -118,9 +118,9 @@ static void setup_variables(void)
          "CPU Core; cached_interpreter|pure_interpreter" },
 #endif
       { "glupen64-43screensize",
-         "4:3 Resolution; 320x240|640x480|960x720|1280x960" },
+         "4:3 Resolution; 320x240|640x480|960x720|1280x960|1600x1200|1920x1440" },
       { "glupen64-169screensize",
-         "16:9 Resolution; 320x180|640x360|960x540|1280x720" },
+         "16:9 Resolution; 320x180|640x360|960x540|1280x720|1920x1080|3840x2160" },
       { "glupen64-aspect",
          "Aspect Ratio; 4:3|16:9" },
       { "glupen64-BilinearMode",
