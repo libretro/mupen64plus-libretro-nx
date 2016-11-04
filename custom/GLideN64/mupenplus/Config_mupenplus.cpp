ab0498817d6f351cc1dceb54dfbb1760a9356185
Logan McNaughton <logan@bacoosta.com>
2016-11-04 08:36:40 -0700

    Load settings from GLideN64.custom.ini
    These setting will override user defined settings.
    
    This is how it works in Project64, these settings are usually required for the particular game to function properly.
    
    Hopefully this will help people that aren't sure what settings are required for each game.

--- custom/GLideN64/mupenplus/Config_mupenplus.cpp
+++ custom/GLideN64/mupenplus/Config_mupenplus.cpp
@@ -11,13 +11,62 @@
 #include "../GBI.h"
 #include "../RSP.h"
 #include "../Log.h"
-extern "C" {
-#include <sys/stat.h>
-#include "osal_files.h"
-#include <dirent.h>
-}
+#include "main/util.h"
 
 Config config;
+
+void LoadCustomSettings()
+{
+	std::string myString = RSP.romname;
+	for (size_t pos = myString.find(' '); pos != std::string::npos; pos = myString.find(' ', pos))
+	{
+		myString.replace(pos, 1, "%20");
+	}
+	for (size_t pos = myString.find('\''); pos != std::string::npos; pos = myString.find('\'', pos))
+	{
+		myString.replace(pos, 1, "%27");
+	}
+	FILE *fPtr;
+	int lineno;
+	bool found = false;
+	char buffer[256];
+	const char *pathname = ConfigGetSharedDataFilepath("GLideN64.custom.ini");
+	if (pathname == NULL || (fPtr = fopen(pathname, "rb")) == NULL)
+	{
+		printf("Unable to open GLideN64 settings file '%s'.", pathname);
+		return;
+	}
+	for (lineno = 1; fgets(buffer, 255, fPtr) != NULL; lineno++)
+	{
+		char *line = buffer;
+		ini_line l = ini_parse_line(&line);
+		switch (l.type)
+		{
+			case INI_SECTION:
+			{
+				if (myString == l.name)
+					found = true;
+				else
+					found = false;
+			}
+			case INI_PROPERTY:
+			{
+				if (found) {
+					if (!strcmp(l.name, "frameBufferEmulation.copyToRDRAM"))
+						config.frameBufferEmulation.copyToRDRAM = atoi(l.value);
+					else if (!strcmp(l.name, "frameBufferEmulation.copyFromRDRAM"))
+						config.frameBufferEmulation.copyFromRDRAM = atoi(l.value);
+					else if (!strcmp(l.name, "frameBufferEmulation.copyDepthToRDRAM"))
+						config.frameBufferEmulation.copyDepthToRDRAM = atoi(l.value);
+					else if (!strcmp(l.name, "frameBufferEmulation.copyAuxToRDRAM"))
+						config.frameBufferEmulation.copyAuxToRDRAM = atoi(l.value);
+					else if (!strcmp(l.name, "frameBufferEmulation.N64DepthCompare"))
+						config.frameBufferEmulation.N64DepthCompare = atoi(l.value);
+				}
+			}
+		}
+	}
+}
 
 void Config_LoadConfig()
 {
@@ -40,4 +89,5 @@ void Config_LoadConfig()
 	config.generalEmulation.enableFragmentDepthWrite = 0;
 #endif
 	config.generalEmulation.hacks = hacks;
+	LoadCustomSettings();
 }
