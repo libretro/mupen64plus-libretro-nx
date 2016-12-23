24baf2860aa47cb08492d312a53f6db7c7d718ff
Logan McNaughton <logan@bacoosta.com>
2016-12-23 07:19:56 -0700

    Use glFlushMappedBufferRange

--- custom/glsm/glsmsym.h
+++ custom/glsm/glsmsym.h
@@ -175,7 +175,6 @@
 #define glBufferStorage             rglBufferStorage
 
 #define GL_MAP_PERSISTENT_BIT         0x0040
-#define GL_MAP_COHERENT_BIT           0x0080
 #define GL_MAP_UNSYNCHRONIZED_BIT     0x0020
 #define GL_MAP_FLUSH_EXPLICIT_BIT     0x0010
 #define GL_DRAW_INDIRECT_BUFFER       0x8F3F
