98ef19872c44938e7f84ad4a2deb25dd98c1487a
radius <andres.430@gmail.com>
2017-03-02 20:30:23 -0500

    reverting 6ac2f76fed1c993c27c037c5d6a9f5359fceb0e9  fixes #6

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -897,14 +897,15 @@ void retro_unload_game(void)
 
 void retro_run (void)
 {
-    libretro_buffer_swapped = false;
+    libretro_swap_buffer = false;
     static bool updated = false;
     if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
         update_controllers();
     glsm_ctl(GLSM_CTL_STATE_BIND, NULL);
     co_switch(game_thread);
-    if (!libretro_buffer_swapped)
-        glsm_ctl(GLSM_CTL_STATE_UNBIND, NULL);
+    glsm_ctl(GLSM_CTL_STATE_UNBIND, NULL);
+    if (libretro_swap_buffer)
+        video_cb(RETRO_HW_FRAME_BUFFER_VALID, retro_screen_width, retro_screen_height, 0);
 }
 
 void retro_reset (void)
