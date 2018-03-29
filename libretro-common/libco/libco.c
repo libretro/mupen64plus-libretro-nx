c68f4eda110db092c6a8baf7e9760d6f08c15787
Aaron Kling <webgeek1234@gmail.com>
2018-03-29 17:04:57 -0500

    Add libco for aarch64

--- libretro-common/libco/libco.c
+++ libretro-common/libco/libco.c
@@ -12,6 +12,8 @@
   #include "ppc.c"
 #elif defined(__GNUC__) && defined(VITA)
   #include "scefiber.c"
+#elif defined(__GNUC__) && defined(__aarch64__)
+  #include "aarch64.c"
 #elif defined(__GNUC__) && (defined(__ARM_EABI__) || defined(__arm__))
   #include "armeabi.c"
 #elif defined(__GNUC__)
