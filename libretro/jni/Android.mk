220921f02eb8cd3f54b26e2d129741d9bf79ec45
Aaron Kling <webgeek1234@gmail.com>
2018-04-07 21:07:28 -0500

    Refactor android jni makefiles
    Also added x86_64 support and fixed x86 support. However, x86
    dynarec is still not pic compliant and will not run on api 23+.

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -1,36 +1,43 @@
 LOCAL_PATH := $(call my-dir)
 
-# Reset flags that the common makefile doesn't properly handle
-DYNAFLAGS    :=
-GLFLAGS      :=
-HAVE_NEON    :=
-SOURCES_C    :=
-SOURCES_CXX  :=
-SOURCES_ASM  :=
-SOURCES_NASM :=
-
 ROOT_DIR     := $(LOCAL_PATH)/../..
 LIBRETRO_DIR := $(ROOT_DIR)/libretro
 INCFLAGS     := -I$(ROOT_DIR)/GLideN64/src/GLideNHQ/inc
 
-PLATFORM_EXT := android
+# Reset flags that the common makefile doesn't properly handle
 platform     := android
+COREFLAGS    :=
+COREASMFLAGS :=
+CORELDLIBS   :=
+DYNAFLAGS    :=
+GLES         :=
+GLFLAGS      :=
+HAVE_NEON    :=
+PNG_PATH     :=
+SOURCES_C    :=
+SOURCES_CXX  :=
+SOURCES_ASM  :=
+SOURCES_NASM :=
 LLE          := 1
+WITH_DYNAREC :=
 
-ifeq ($(TARGET_ARCH),arm)
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
   WITH_DYNAREC := arm
   HAVE_NEON := 1
-  STATIC_LIBS := $(ROOT_DIR)/custom/android/arm/libpng.a
-endif
-
-ifeq ($(TARGET_ARCH),arm64)
+  PNG_PATH := $(ROOT_DIR)/custom/android/arm/libpng.a
+else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
   WITH_DYNAREC := arm64
-  STATIC_LIBS := $(ROOT_DIR)/custom/android/arm64/libpng.a
-endif
-
-ifeq ($(TARGET_ARCH),x86)
+  PNG_PATH := $(ROOT_DIR)/custom/android/arm64/libpng.a
+else ifeq ($(TARGET_ARCH_ABI),x86)
+  # X86 dynarec isn't position independent, so it fails to build on newer ndks.
+  # No warn shared textrel allows it to build, but still won't allow it to run on api 23+.
   WITH_DYNAREC := x86
-  STATIC_LIBS := $(ROOT_DIR)/custom/android/x86/libpng.a
+  PNG_PATH := $(ROOT_DIR)/custom/android/x86/libpng.a
+  COREASMFLAGS := -f elf -d ELF_TYPE
+  CORELDLIBS := -Wl,-no-warn-shared-textrel
+else ifeq ($(TARGET_ARCH_ABI),x86_64)
+  WITH_DYNAREC := x86_64
+  PNG_PATH := $(ROOT_DIR)/custom/android/x86_64/libpng.a
 endif
 
 ifeq ($(GLES3),1)
@@ -44,14 +51,25 @@ include $(ROOT_DIR)/Makefile.common
 
 COREFLAGS += -D__LIBRETRO__ -DUSE_FILE32API -DM64P_PLUGIN_API -DM64P_CORE_PROTOTYPES -D_ENDUSER_RELEASE -DSINC_LOWER_QUALITY -DMUPENPLUSAPI -DTXFILTER_LIB -D__VEC4_OPT $(INCFLAGS) $(GLFLAGS) $(DYNAFLAGS) -DANDROID -DEGL_EGLEXT_PROTOTYPES -DOS_LINUX
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := retro
-LOCAL_SRC_FILES = $(SOURCES_CXX) $(SOURCES_C) $(SOURCES_ASM) $(SOURCES_NASM)
-LOCAL_CPPFLAGS := -std=gnu++11 $(COREFLAGS)
-LOCAL_CFLAGS := $(COREFLAGS)
-LOCAL_LDFLAGS := -Wl,-Bsymbolic -shared
-LOCAL_LDLIBS :=  -lz -llog -lEGL -latomic $(GLLIB) $(STATIC_LIBS)
-LOCAL_ARM_NEON := true
-LOCAL_CPP_FEATURES := exceptions
-include $(BUILD_SHARED_LIBRARY)
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
 
+include $(CLEAR_VARS)
+LOCAL_MODULE    := png
+LOCAL_SRC_FILES := $(PNG_PATH)
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := retro
+LOCAL_SRC_FILES        := $(SOURCES_CXX) $(SOURCES_C) $(SOURCES_ASM) $(SOURCES_NASM)
+LOCAL_ASMFLAGS         := $(COREASMFLAGS)
+LOCAL_CPPFLAGS         := -std=gnu++11 $(COREFLAGS)
+LOCAL_CFLAGS           := $(COREFLAGS)
+LOCAL_LDFLAGS          := -Wl,-version-script=$(LIBRETRO_DIR)/link.T
+LOCAL_LDLIBS           := -lz -llog -lEGL $(GLLIB) $(CORELDLIBS)
+LOCAL_STATIC_LIBRARIES := png
+LOCAL_CPP_FEATURES     := exceptions
+LOCAL_ARM_NEON         := true
+include $(BUILD_SHARED_LIBRARY)
