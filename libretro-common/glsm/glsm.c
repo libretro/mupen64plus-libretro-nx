deb881ca5b2fd7a0219ba9736ecdfa66f2600111
M4xw <m4x@m4xw.net>
2019-05-20 15:56:45 +0200

    Honor OpenGL Core Profile if defined

--- libretro-common/glsm/glsm.c
+++ libretro-common/glsm/glsm.c
@@ -3295,13 +3295,17 @@ static bool glsm_state_ctx_init(glsm_ctx_params_t *params)
    hw_render.context_type       = RETRO_HW_CONTEXT_OPENGLES2;
 #endif
 #else
+#if defined(CORE) && !defined(HAVE_LIBNX)
+   hw_render.context_type       = RETRO_HW_CONTEXT_OPENGL_CORE;
+   hw_render.version_major      = 3;
+   hw_render.version_minor      = 3;
+#else
    hw_render.context_type       = RETRO_HW_CONTEXT_OPENGL;
-   if (params->context_type != RETRO_HW_CONTEXT_NONE)
-      hw_render.context_type    = params->context_type;
    if (params->major != 0)
       hw_render.version_major   = params->major;
    if (params->minor != 0)
       hw_render.version_minor   = params->minor;
+#endif // defined(CORE) && !defined(HAVE_LIBNX)
 #endif
 
    hw_render.context_reset      = params->context_reset;
