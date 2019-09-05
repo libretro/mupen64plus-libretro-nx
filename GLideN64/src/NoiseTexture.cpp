f58d16e94a5ffa18a8ba3c05a5cd20020e0b3a9d
Brad Miller <mudlord@rebote.net>
2019-09-05 21:05:18 +1000

    Fix for 20 second pause on some systems. (#93)
    * Fix for 20 second pause on some systems.

--- GLideN64/src/NoiseTexture.cpp
+++ GLideN64/src/NoiseTexture.cpp
@@ -34,27 +34,25 @@ NoiseTexture::NoiseTexture()
 		m_pTexture[i] = nullptr;
 }
 
-static
-u32 Rand(u32 rand_value)
+
+inline u32 irand()
 {
-#ifdef MINGW
-	rand_s(&rand_value);
-#else
-	rand_value = rand();
-#endif
-	return rand_value;
+	static u32 iseed = 1;
+	iseed *= 0x343fd;
+	iseed += 0x269ec3;
+	return ((iseed >> 16) & 0x7fff);
 }
+
 
 static
 void FillTextureData(u32 _seed, NoiseTexturesData * _pData, u32 _start, u32 _stop)
 {
-	srand(_seed);
 	for (u32 i = _start; i < _stop; ++i) {
 		auto & vec = _pData->at(i);
 		const size_t sz = vec.size();
 		u32 rand_value(0U);
 		for (size_t t = 0; t < sz; ++t) {
-			rand_value = Rand(rand_value);
+			rand_value = irand();
 			vec[t] = rand_value & 0xFF;
 		}
 	}
@@ -136,7 +134,7 @@ void NoiseTexture::update()
 
 	u32 rand_value(0U);
 	while (m_currTex == m_prevTex) {
-		rand_value = Rand(rand_value);
+		rand_value = irand();
 		m_currTex = rand_value % NOISE_TEX_NUM;
 	}
 	m_prevTex = m_currTex;
