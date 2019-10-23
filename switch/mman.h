8c6aa5864c7c4c2067a99a8a33b597d129b20aae
M4xw <m4x@m4xw.net>
2019-10-23 23:28:11 +0200

    Some nx jit hacks

--- switch/mman.h
+++ switch/mman.h
@@ -21,26 +21,18 @@ extern "C"
 #define MAP_ANONYMOUS 0x20
 
 #define MAP_FAILED ((void *)-1)
-
-Jit dynarec_jit;
-void *jit_rx_addr = 0;
-void *jit_rw_addr = 0;
-size_t jit_len = 0;
+void* ptr_rw = NULL;
 
 static inline void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
     (void)fd;
     (void)offset;
 
-    if (R_SUCCEEDED(jitCreate(&dynarec_jit, len)))
+    size_t size = (len + 0xFFF) &~ 0xFFF;
+	ptr_rw = virtmemReserve(size);
+    if (R_SUCCEEDED(svcMapProcessMemory(ptr_rw, envGetOwnProcessHandle(), (u64)addr, size)))
     {
-        jit_len = dynarec_jit.size;
-        jit_rw_addr = jitGetRwAddr(&dynarec_jit);
-        jit_rx_addr = jitGetRxAddr(&dynarec_jit);
-        
-        printf("[NXJIT]: Jit Initialized: RX %p, RW %p\n", jit_rx_addr, jit_rw_addr);
-
-        return jit_rw_addr;
+        return ptr_rw;
     }
     else
     {
@@ -56,10 +48,8 @@ static inline int mprotect(void *addr, size_t len, int prot)
 
 static inline int munmap(void *addr, size_t len)
 {
-    jitClose(&dynarec_jit);
-    jit_rx_addr = jit_rw_addr = NULL;
-    jit_len = 0;
-
+    size_t size = (len + 0xFFF) &~ 0xFFF;
+    svcUnmapProcessMemory(ptr_rw, envGetOwnProcessHandle(), (u64)addr, size);
     printf("[NXJIT]: Jit closed\n");
     
     return 0;
