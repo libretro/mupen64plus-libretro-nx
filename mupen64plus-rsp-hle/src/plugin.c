7630fe3b95d67e2b5ed6a5ea99c276ed05c06e80
M4xw <m4x@m4xw.net>
2020-02-04 12:26:28 +0100

    git subrepo clone --force https://github.com/libretro/mupen64plus-rsp-hle.git mupen64plus-rsp-hle
    subrepo:
      subdir:   "mupen64plus-rsp-hle"
      merged:   "8510c797"
    upstream:
      origin:   "https://github.com/libretro/mupen64plus-rsp-hle.git"
      branch:   "master"
      commit:   "8510c797"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

--- mupen64plus-rsp-hle/src/plugin.c
+++ mupen64plus-rsp-hle/src/plugin.c
@@ -41,7 +41,7 @@
 #define CONFIG_PARAM_VERSION     1.00
 
 #define RSP_API_VERSION   0x20000
-#define RSP_HLE_VERSION        0x020500
+#define RSP_HLE_VERSION        0x020509
 #define RSP_PLUGIN_API_VERSION 0x020000
 
 /* local variables */
@@ -179,6 +179,28 @@ EXPORT m64p_error CALL hlePluginShutdown(void)
 
 EXPORT unsigned int CALL hleDoRspCycles(unsigned int Cycles)
 {
+    /* Since RSP plugin API doesn't provide a "RomOpen" function
+     * we implement one with a flag inside DoRspCycle.
+     * once_per_rom is reset in RomClose. */
+    if (!g_hle.once_per_rom) {
+
+        /* Extract ROM product code so we can roughly identify ROM */
+        m64p_rom_header rom_header;
+        CoreDoCommand(M64CMD_ROM_GET_HEADER, sizeof(rom_header), &rom_header);
+
+        /* XXX: rom_header structure is WRONG,
+         * so we recompose proper product code from exposed m64p_rom_header */
+        g_hle.product_code
+            = ((uint32_t)rom_header.Manufacturer_ID & UINT32_C(0xff000000))
+            | ((uint32_t)rom_header.Cartridge_ID & UINT32_C(0x00ff)) << 16
+            | ((uint32_t)rom_header.Cartridge_ID & UINT32_C(0xff00))
+            | ((uint32_t)rom_header.Country_code & UINT32_C(0x00ff));
+
+        HleWarnMessage(g_hle.user_defined, "Product Code = %08x", g_hle.product_code);
+
+        g_hle.once_per_rom = 1;
+    }
+
     hle_execute(&g_hle);
     return Cycles;
 }
