c6cbb4b77f1c76ef463de807dbdbc9addf22a73d
lucinda lovebuny <landfillbaby69@gmail.com>
2025-08-05 12:50:19 +0100

    put it in a different place

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -494,9 +494,6 @@ static void* EmuThreadFunction(void* param)
 
 static void reinit_gfx_plugin(void)
 {
-    if (log_cb)
-        log_cb(RETRO_LOG_DEBUG, "reinit_gfx_plugin called with first_context_reset = %s.\n",
-            first_context_reset ? "true" : "false");
 #ifdef HAVE_PARALLEL_RDP
     if (current_rdp_type == RDP_PLUGIN_PARALLEL)
     {
@@ -1807,7 +1804,8 @@ void context_reset(void)
 {
     if(current_rdp_type == RDP_PLUGIN_GLIDEN64)
     {
-       log_cb(RETRO_LOG_DEBUG, CORE_NAME ": context_reset()\n");
+       log_cb(RETRO_LOG_DEBUG, CORE_NAME ": context_reset(), first_context_reset = %s.\n",
+            first_context_reset ? "true" : "false");
        glsm_ctl(GLSM_CTL_STATE_CONTEXT_RESET, NULL);
        if (!context_setup_first_init)
        {
