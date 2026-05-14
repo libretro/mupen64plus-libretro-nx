libretro/mgba

Add support for 16KB pages on Android.

libretro-build/jni/Android.mk
@@ -18,6 +18,6 @@ LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(SOURCES_C) $(SOURCES_CXX)
 LOCAL_CPPFLAGS  := -O3 $(COREFLAGS)
 LOCAL_CFLAGS    := -O3 $(COREFLAGS)
-LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T,-z,max-page-size=16384
 LOCAL_ARM_MODE  := arm
 include $(BUILD_SHARED_LIBRARY)

libretro/mupen64plus-libretro-nx

libretro/jni/Android.mk
@@ -85,7 +85,7 @@ LOCAL_SRC_FILES        := $(SOURCES_CXX) $(SOURCES_C) $(SOURCES_ASM) $(SOURCES_N
 LOCAL_ASMFLAGS         := $(COREASMFLAGS)
 LOCAL_CPPFLAGS         := -std=gnu++11 $(CXXFLAGS) $(COREFLAGS)
 LOCAL_CFLAGS           := $(CFLAGS) $(COREFLAGS)
-LOCAL_LDFLAGS          := -Wl,-version-script=$(LIBRETRO_DIR)/link.T
+LOCAL_LDFLAGS          := -Wl,-version-script=$(LIBRETRO_DIR)/link.T,-z,max-page-size=16384
 LOCAL_LDLIBS           := -llog -lEGL $(GLLIB) $(CORELDLIBS)
 LOCAL_STATIC_LIBRARIES := 
 LOCAL_CPP_FEATURES     := exceptions